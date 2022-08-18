54b53c3f67f91dcaf85c6899e266707cde59ac14
Raissa C. Correia <raisssa.correia@gmail.com>
2022-08-18 17:24:44 -0300

    doing first chapter cracking book

---
+++ CrackingBook/01 - ArraysStrings.ts
@@ -0,0 +1,77 @@
+function IsUnique(str: String) {
+
+    //supposing is ASCII
+    if (str.length > 128) return false;
+
+    //space complexity O(n)
+    let char_set:Array<Boolean> = [];
+
+    //time complexity O(n)
+    [...str].forEach(c => {
+        if (char_set[c]) return false;
+        char_set[c] = true;
+    });
+    return true;
+}
+
+function isUniqueBitWise(str: String) {
+    let checker = 0;
+    for (let i = 0; i < str.length; i++) {
+        let val = str.charCodeAt(i) - 'a'.charCodeAt(0);
+        if ((checker & (1 << val)) > 0) return false;
+        checker |= (1 << val);
+    }
+    return true;
+}
+
+//without sorting, ascii or unicode? case sensitive or not?
+function checkPermutation(str1: String, str2: String) {
+    if (str1.length !== str2.length) return false;
+    let char_set:Array<Boolean> = [];
+    [...str1].forEach(c => {
+        char_set[c] = true;
+    }
+    );
+    for (let i = 0; i < str2.length; i++) {
+        let c = str2.charAt(i);
+        if (!char_set[c]) return false;
+        char_set[c] = false;
+    }
+    return true;
+}
+
+function urlify(str: String) {
+
+    let spaceCount = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == ' ') spaceCount++;
+    }
+    let trueLength = str.length + spaceCount * 2;
+
+    let result = '';
+    for (let i = 0; i < trueLength; i++) {
+        if (str.charAt(i) === ' ') {
+            result += '%20';
+        } else {
+            result += str.charAt(i);
+        }
+    }
+    return result;
+}
+
+/*function palindromePermutation (str: String) {
+    let char_set:Array<Boolean> = [];
+    for (let i = 0; i < str.length; i++) {
+        let c = str.charAt(i);
+        if (char_set[c]) {
+            char_set[c] = false;
+        } else {
+            char_set[c] = true;
+        }
+    }
+    let count = 0;
+    for (let i = 0; i < char_set.length; i++) {
+        if (char_set[i]) count++;
+    }
+    return count <= 1;
+}*/
